a486141966329a0a2a505169cd0931d3ae995310
leleg <lelegsoares@gmail.com>
2026-02-26 12:41:00 -0300

    Comentando Program.java
    Adicionando comentarios

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -11,21 +11,22 @@ public class Program {
 	public static void main(String[] args) {
 		
 		try (Scanner sc = new Scanner(System.in);) {
+			int idade = sc.nextInt(); // Idade vem primeiro por conta do Buffer
 			String nome = sc.nextLine();
-			int idade = sc.nextInt();
 			
-			Pessoa pessoa = new Pessoa(nome, idade);
+			
+			Pessoa pessoa = new Pessoa(nome, idade); // Aplica regra de negócio no construtor
 			System.out.printf(" %s tem %d anos.\n", pessoa.getName(), pessoa.getIdade());
 		}
-		catch (DomainException e) {
+		catch (DomainException e) { // Exceção de regras de negócio
 			System.out.print(e.getMessage());
 		}
-		catch (InputMismatchException e) {
+		catch (InputMismatchException e) { // Exceção de Entrada inválida;
 			System.out.print("Deu ruim");
 		}
-		finally {
+		finally { // Geralmente usado para limpeza tecnica
 			System.out.print("\nPrograma Finalizado.");
 		}
 	}
-
 }
+
