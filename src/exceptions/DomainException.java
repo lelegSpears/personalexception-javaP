10af731f671c60e8db55a698e052c1c14b31d127
leleg <lelegsoares@gmail.com>
2026-02-26 13:00:15 -0300

    Update DomainException.java

--- src/exceptions/DomainException.java
+++ src/exceptions/DomainException.java
@@ -4,9 +4,10 @@ public class DomainException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
 
-	public DomainException(String message) {
-		super(message);
-	}
+	public DomainException(String message, Throwable cause) { // Mensagem e Causa do Erro
+    super(message, cause);
+}
 	
 
 }
+
